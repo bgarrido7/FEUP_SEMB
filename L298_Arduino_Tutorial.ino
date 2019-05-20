58f8d26e0f4eadebf5b095226421cbb7fbdf0b56
Bia Garrido <36662037+bgarrido7@users.noreply.github.com>
2019-05-20 17:42:54 +0100

    movimento base das rodas done

--- L298_Arduino_Tutorial.ino
+++ L298_Arduino_Tutorial.ino
@@ -1,4 +1,3 @@
-
 /*Motor B*/
  int motorPin4  = 9;  /* IN4 */
  int motorPin3  = 10;  /* IN3 */
@@ -9,41 +8,46 @@
 
 void setup(){
 
-    /*Set pins as outputs*/
+/*Set pins as outputs*/
     pinMode(motorPin1, OUTPUT);
     pinMode(motorPin2, OUTPUT);
     pinMode(motorPin3, OUTPUT);
     pinMode(motorPin4, OUTPUT);
 
+    
+//CARRO ANDA PARA A FRENTE
     /*Motor da esquerda frente*/
     analogWrite(motorPin1, 0);
     analogWrite(motorPin3, 0);
-    analogWrite(motorPin2, 180); /* velocdade vai até 255 */
-    analogWrite(motorPin4, 0);
-    delay(1000);  
+    analogWrite(motorPin2, 255);    /* velocdade vai até 255 */
+   //analogWrite(motorPin4, 0);
+   //delay(1000);  
+    
+    /*Motor da direita frente*/
+    analogWrite(motorPin4, 255);
+  //analogWrite(motorPin2, 0);
+    analogWrite(motorPin3, 0);
+    analogWrite(motorPin1, 0);
+    delay(1000);
 
+
+//CARRO ANDA PARA TRAS    
     /*Motor da esquerda tras*/
     analogWrite(motorPin4, 0);
     analogWrite(motorPin2, 0);
-    analogWrite(motorPin3, 0);
-    analogWrite(motorPin1, 180);
-    delay(1000);  
-
-    /*Motor da direita frente*/
-    analogWrite(motorPin4, 180);
-    analogWrite(motorPin2, 0);
-    analogWrite(motorPin3, 0);
-    analogWrite(motorPin1, 0);
-    delay(1000);
+  //analogWrite(motorPin3, 0);
+    analogWrite(motorPin1, 255);
+  //delay(1000);  
 
     /*Motor da direita tras*/
-    analogWrite(motorPin1, 0);
-    analogWrite(motorPin3, 180);
+  //analogWrite(motorPin1, 0);
+    analogWrite(motorPin3, 255);
     analogWrite(motorPin2, 0);
     analogWrite(motorPin4, 0);
     delay(1000);
+
     
-    /*Stop all motors*/
+/*Stop all motors*/
     analogWrite(motorPin1, 0);
     analogWrite(motorPin2, 0);
     analogWrite(motorPin3, 0);
